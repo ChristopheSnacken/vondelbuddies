59ece3c92e47d6e83d11422e11eb9f780e7c1088
Sophie Janson <sophie@sophiejanson.com>
2018-06-18 20:46:50 +0200

    Added link on welcome page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Router } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.css';
+
+import Welcome from './components/Welcome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello VondelBuddies!</h1>
+        <Route exact path="/" component={Welcome} />
       </div>
     );
   }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Welcome(props) {
-  return props
+  return <Link to={'./interests'}><span className="button">Get started</span></Link>
 }
