526066656b17d5a103e1152af31046f8f41b7847
Sophie Janson <sophie@sophiejanson.com>
2018-06-19 11:09:58 +0200

    made dev branch

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -12,44 +12,26 @@ const styles = {
     backgroundColor: '#B3DC4A',
   },
 };
-<<<<<<< HEAD
-
 
 function Matches(props) {
-
-=======
-
-
-function Matches(props) {
-    
->>>>>>> c5c4d7d672c84254df7c732ef0472207019cb047
-    return (
-        <BindKeyboardSwipeableViews enableMouseEvents>
-            {
-                props.matches.map((match) => {
-                    return(
-                        <div key={match.id} style={Object.assign({}, styles.slide)}>
-                            <ul>
-                                <li>{match.name}</li>
-                                <li>Age: {match.age}</li>
-                                <li>Activity: {match.activity}</li>
-                            </ul>
-                            <button onClick={()=>props.accept(match.id)}>Accept</button>
-                            <button onClick={()=>props.reject(match.id)}>Reject</button>
-                        </div>
-                    )
-                })
-<<<<<<< HEAD
-            }
-=======
-            }    
->>>>>>> c5c4d7d672c84254df7c732ef0472207019cb047
-         </BindKeyboardSwipeableViews>
+  return (
+    <BindKeyboardSwipeableViews enableMouseEvents>
+        {
+          props.matches.map((match) => {
+              return(
+                <div key={match.id} style={Object.assign({}, styles.slide)}>
+                  <ul>
+                    <li>{match.name}</li>
+                    <li>Age: {match.age}</li>
+                    <li>Activity: {match.activity}</li>
+                  </ul>
+                  <button onClick={()=>props.accept(match.id)}>Accept</button>
+                  <button onClick={()=>props.reject(match.id)}>Reject</button>
+                </div>
+              )
+          })
+        }
+     </BindKeyboardSwipeableViews>
     );
 }
-
-<<<<<<< HEAD
 export default Matches;
-=======
-export default Matches;
->>>>>>> c5c4d7d672c84254df7c732ef0472207019cb047
