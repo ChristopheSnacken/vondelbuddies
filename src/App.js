c5c4d7d672c84254df7c732ef0472207019cb047
Martijnbartelson <martijnbar@gmail.com>
2018-06-19 10:49:35 +0200

    changed routing in app.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import './App.css';
 import MatchesContainer from './components/MatchesContainer'
-import Welcome from './components/Welcome'
-
+import WelcomeContainer from './components/WelcomeContainer'
+import UserProfilesContainer from './components/UserProfilesContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello VondelBuddies!</h1>
-        <Route exact path="/" component={Welcome} />
+        <header>
 
-        <Route exact path="/matches" component={MatchesContainer} />
-
+        </header>
+        <main>
+          <Route exact path="/" component={WelcomeContainer} />
+          <Route exact path="/profile" component={UserProfilesContainer} />
+          <Route exact path="/matches" component={MatchesContainer} />
+        </main>
+      
       </div>
-    );
+    )
   }
 }
 
