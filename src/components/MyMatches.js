589ccfae55204e68b1ac992944d1cdb57d537a12
Sophie Janson <sophie@sophiejanson.com>
2018-06-20 16:25:40 +0200

    added css

--- src/components/MyMatches.js
+++ src/components/MyMatches.js
@@ -6,10 +6,9 @@ export default function MyMatches(props) {
   return (
     <div className="matches">
       <img className="profile-pic" src={require(`./../img/userphotos/profile_pic.png`)} alt="Current user's face" />
-      <h3>{activeUser.name}</h3>
-      <div className="my-matches">
-        <h4>My matches</h4>
-        <hr className="divider" />
+      <h1 className="profile-name">{activeUser.name}</h1>
+        <h3 className="my-matches-title">My matches</h3>
+        <div className="my-matches">
         {matches.length > 0 && matches.map(match => <img className="match-pic" src={require(`./../img/userphotos/uid-${match.img}.png`)} alt={match.name + "s face"} />)}
         {matches.length === 0 && `You don't have any matches yet.`}
       </div>
