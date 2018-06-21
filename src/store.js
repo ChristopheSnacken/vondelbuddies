338f829397a88797d2fa3cb22d326e28bbf8e3f3
Sophie Janson <sophie@sophiejanson.com>
2018-06-21 09:10:00 +0200

    bugfix

--- src/store.js
+++ src/store.js
@@ -3,8 +3,7 @@ import reducer from './reducers'
 import ReduxThunk from 'redux-thunk'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const enhancer = compose(applyMiddleware(ReduxThunk), devTools)
 
-const store = createStore(reducer, enhancer)
+const store = createStore(reducer, devTools)
 
 export default store
