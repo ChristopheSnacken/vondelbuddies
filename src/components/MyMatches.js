dbd3e119dffc72e2dbc6c43ccb67f4950044679a
Sophie Janson <sophie@sophiejanson.com>
2018-06-21 11:51:02 +0200

    connected mymatches

--- src/components/MyMatches.js
+++ src/components/MyMatches.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import './MyMatches.css'
+import { Link } from 'react-router-dom'
 
 export default function MyMatches(props) {
   const { activeUser, matches } = props
@@ -9,7 +10,7 @@ export default function MyMatches(props) {
       <h1 className="profile-name">{activeUser.name}</h1>
         <h3 className="my-matches-title">My matches</h3>
         <div className="my-matches">
-        {matches.length > 0 && matches.map(match => <img className="match-pic" src={require(`./../img/userphotos/uid-${match.img}.png`)} alt={match.name + "s face"} />)}
+        {matches.length > 0 && matches.map(match => <Link key={match.id} params={{index: 0}} to={{pathname: `/matches`, query: {index: match.id}}}><img className="match-pic" src={require(`./../img/userphotos/uid-${match.img}.png`)} alt={match.name + "s face"} /></Link>)}
         {matches.length === 0 && `You don't have any matches yet.`}
       </div>
     </div>
--- src/components/WelcomeContainer.js
+++ src/components/WelcomeContainer.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import OnboardingMessage from './OnboardingMessage'
-import WelcomeBackMessage from './WelcomeBackMessage'
 import { Link } from 'react-router-dom'
 
 export default class Welcome extends React.PureComponent {
