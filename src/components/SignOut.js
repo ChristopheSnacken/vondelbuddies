534b603ba579d98385fea1e239b53145f81dc3e6
Sophie Janson <sophie@sophiejanson.com>
2018-06-22 09:54:56 +0200

    added horizontal scroll

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { auth } from '../firebase';
 
 const SignOutButton = () =>
-  <button className="btn btn-small"
+  <button className="btn btn-small sign-out-button"
     type="button"
     onClick={auth.doSignOut}
   >
